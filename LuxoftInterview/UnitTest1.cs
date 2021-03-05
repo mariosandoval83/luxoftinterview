ea85efeae2de7d88ba77bfc283ee40bed04d10a6
Mario Sandoval <mario.a.sandoval@outlook.com>
2021-03-04 20:31:09 -0600

    updated script

--- LuxoftInterview/UnitTest1.cs
+++ LuxoftInterview/UnitTest1.cs
@@ -31,7 +31,7 @@ namespace LuxoftInterview
             mouseAction.MoveToElement(womenTitle).Perform();
 
             //Click Summer dresses link
-            IWebElement summerDressesSection = driver.FindElement(By.XPath("//*[@id='block_top_menu']/ul/li[1]/ul/li[2]/ul/li[3]/a"));            
+            IWebElement summerDressesSection = driver.FindElement(By.XPath("//*[@id='block_top_menu']/ul/li[1]/ul/li[2]/ul/li[3]/a"));
             summerDressesSection.Click();
 
             //Wait for page to load
@@ -40,37 +40,48 @@ namespace LuxoftInterview
             IWebElement summerDressTitle = driver.FindElement(By.XPath("//span[@class='cat-name']"));
             bool isSummerDressTittleDisplayed = summerDressTitle.Displayed;
             Assert.IsTrue(isSummerDressTittleDisplayed, "Summer Dress page was displayed");
-            
+            Thread.Sleep(2000);
+
             //Select 2nd summer dress
-            IWebElement secondSummerDress = driver.FindElement(By.XPath("/html/body/div/div[2]/div/div[3]/div[2]/ul/li[2]/div/div[2]/h5/a"));
-            //secondSummerDress.SendKeys("");
-            mouseAction.MoveToElement(secondSummerDress).Perform();
+            IWebElement secondSummerDress = driver.FindElement(By.XPath("/html/body/div/div[2]/div/div[3]/div[2]/ul/li[2]"));
+            secondSummerDress.Click();
+            Thread.Sleep(2000);
 
             //Click add to cart button
-            IWebElement addToCartButton = driver.FindElement(By.XPath("//span[text()='Add to cart']"));
+            IWebElement addToCartButton = driver.FindElement(By.XPath("/html/body/div/div[2]/div/div[3]/div[2]/ul/li[2]/div/div[2]/div[2]/a[1]/span"));
             addToCartButton.Click();
+            Thread.Sleep(2000);
 
             //Validate item added to cart
-            IWebElement dressAddedToCart = driver.FindElement(By.XPath("/html/body/div/div[1]/header/div[3]/div/div/div[4]/div[1]/div[1]/h2/text()"));
-            bool isTextDisplayed = dressAddedToCart.Displayed;
-            Assert.IsTrue(isTextDisplayed, "Text confirmation was displayed");
+            IWebElement dressAddedToCart = driver.FindElement(By.XPath("//h2/preceding::div[@class='layer_cart_product col-xs-12 col-md-6']"));
+            bool isTextDisplayed = dressAddedToCart.Text.Contains("Product successfully added");
+            if(dressAddedToCart.Text.Contains("Product successfully added") == true)
+            { 
+                Console.WriteLine("Text confirmation was displayed ");
+            }
+            Assert.IsTrue(isTextDisplayed, "Text confirmation was not displayed");
 
             //Close Modal Window
             IWebElement closeModalButton = driver.FindElement(By.XPath("//span[@class='cross' and @title='Close window']"));
             closeModalButton.Click();
+            Thread.Sleep(2000);
 
             //Hover shopping cart
-            IWebElement shoppingCartDropdown = driver.FindElement(By.XPath("//a[@title='View my shopping cart'"));
-            mouseAction.MoveToElement(shoppingCartDropdown).Perform();
+            IWebElement shoppingCartDropdown = driver.FindElement(By.XPath("//b[text()='Cart']"));
+            Actions cartFocus = new Actions(driver);
+            cartFocus.MoveToElement(shoppingCartDropdown).Perform();
+            Thread.Sleep(2000);
 
             //Click remove dress from cart
             IWebElement removeDressButton = driver.FindElement(By.XPath("//a[@rel='nofollow']/preceding::span[@class='remove_link']"));
             removeDressButton.Click();
+            Thread.Sleep(4000);
 
             //Assert dress was deleted
             IWebElement emptyCartText = driver.FindElement(By.XPath("//span[text()='(empty)']"));
             bool isEmptyCartTextDisplayed = emptyCartText.Displayed;
-            Assert.IsTrue(isEmptyCartTextDisplayed, "Dress was removed from cart");
+            Assert.IsTrue(isEmptyCartTextDisplayed, "Dress was not removed from cart");
+
         }
 
         [TestCleanup]
